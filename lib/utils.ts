9a1f16440f11b666be8a116ada68cfd4ecba5ce5
nerdawitz <sakshamsharma98@gmail.com>
2024-09-15 06:11:47 -0400

    implement form authentication, database and backend with appwrite

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,8 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod"
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -193,3 +195,19 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+export const authFormSchema = (type: string) => z.object({
+  // sign-up
+  firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
+  postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(7),
+  dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  sin: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+
+  // sign-in & sign-up
+  email: z.string().email(),
+  password: z.string().min(8),
+})
